d5c4b24711cc8a08db4d3bc92849b629b1be56ec
ryoma55 <ryouma55saki@gmail.com>
2025-07-04 15:50:27 +0900

    機能修正

--- schedule_app/common.py
+++ schedule_app/common.py
@@ -1,15 +1,19 @@
 def now_quarter():
     import datetime
     now = datetime.datetime.now()
-    if 4 <= now.month and now.month <= 5:
-        quarter = 'Q1'
-    elif 6 <= now.month and now.month <= 7:
-        quarter = 'Q2'
-    elif 9 <= now.month and now.month or (now.month == 11 and now.day <= 15):
-        quarter = 'Q3'
-    else:
-        quarter = 'Q4'
-    return quarter
+    quarter ={
+    "Q1":({"month":4,"day":1}, {"month":5, "day":31}),
+    "Q2":({"month":6,"day":1}, {"month":7, "day":31}),
+    "Q3":({"month":9,"day":1}, {"month":11, "day":15}),
+    "Q4":({"month":11,"day":16}, {"month":3, "day":31})
+    }
+    for key, value in quarter.items():    
+        start, end = value
+        if (start['month'] == now.month and start['day'] <= now.day) or \
+           (end['month'] == now.month and now.day <= end['day']) or \
+           (start['month'] < now.month < end['month']):
+            return key
+    return "Q1"  # Default to Q4 if no match found
 
 
 def period_array():
